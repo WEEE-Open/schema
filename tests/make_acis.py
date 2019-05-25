3d387d3938302bf47221622f2713c81c9e6fddcb
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2019-05-26 00:36:08 +0200

    Nextcloud account for user provisioning
    With similar permissions to WSO2IS, so no additional tests... maybe i'll add them later

--- tests/make_acis.py
+++ tests/make_acis.py
@@ -36,20 +36,21 @@ def acis():
 	print(f"""      -
         dn: "ou=People,{suffix}"
         acis:""")
-	print_aci(make_aci('Allow WSO2IS to read users', ('targetfilter = "(uid=*)"', 'targetattr = "objectClass || memberOf || dn || cn || uid || telegramID || createTimestamp || creatorsName || entrydn || entryid || hasSubordinates || modifiersName || modifyTimestamp || nsUniqueId || numSubordinates || parentid || subschemaSubentry"'), {'read', 'search', 'compare'}, f'userdn = "ldap:///cn=WSO2IS,ou=Services,{suffix}"'))
+	print_aci(make_aci('Allow WSO2IS to read users', ('targetfilter = "(uid=*)"', 'targetattr = "objectClass || memberOf || dn || cn || uid || mail || telegramID || createTimestamp || creatorsName || entrydn || entryid || hasSubordinates || modifiersName || modifyTimestamp || nsUniqueId || numSubordinates || parentid || subschemaSubentry"'), {'read', 'search', 'compare'}, f'userdn = "ldap:///cn=WSO2IS,ou=Services,{suffix}"'))
+	print_aci(make_aci('Allow Nextcloud to read users', ('targetfilter = "(uid=*)"', 'targetattr = "objectClass || memberOf || dn || cn || uid || mail || createTimestamp || creatorsName || entrydn || entryid || hasSubordinates || modifiersName || modifyTimestamp || nsUniqueId || numSubordinates || parentid || subschemaSubentry"'), {'read', 'search', 'compare'}, f'userdn = "ldap:///cn=nextcloud,ou=Services,{suffix}"'))
 	print_aci(make_aci('Allow WSO2IS to change passwords', ('targetfilter = "(uid=*)"', 'targetattr = "userPassword"'), {'write'}, f'userdn = "ldap:///cn=WSO2IS,ou=Services,{suffix}"'))
 	print_aci(make_aci('Allow users to change their password', ('targetfilter = "(uid=*)"', 'targetattr = "userPassword"'), {'write'}, f'userdn = "ldap:///self"'))
 
-	print_aci(make_aci('Allow HR to read users', ('targetfilter = "(uid=*)"', 'targetattr = "createTimestamp || creatorsName || modifiersName || modifyTimestamp || objectClass || memberOf || uid || sn || schacPersonalUniqueCode || degreeCourse || schacDateOfBirth || schacPlaceOfBirth || mobile || mail || safetyTestDate || telegramID || telegramNickname || description"'), {'read', 'search', 'compare'}, f'groupdn = "ldap:///cn=HR,ou=Groups,{suffix}"'))
-	# limits possible objectClass values, but doesn't allow to add accounts with less than all these values
+	print_aci(make_aci('Allow HR to read users', ('targetfilter = "(uid=*)"', 'targetattr = "createTimestamp || creatorsName || modifiersName || modifyTimestamp || objectClass || memberOf || uid || sn || schacPersonalUniqueCode || degreeCourse || schacDateOfBirth || schacPlaceOfBirth || mobile || mail || safetyTestDate || telegramID || telegramNickname || description || nsUniqueId"'), {'read', 'search', 'compare'}, f'groupdn = "ldap:///cn=HR,ou=Groups,{suffix}"'))
 	print_aci(make_aci('Allow HR to add, edit, delete users', ('targetfilter="(&(uid=*)(objectClass=top)(objectClass=inetOrgPerson)(objectClass=organizationalPerson)(objectClass=person)(objectClass=schacPersonalCharacteristics)(objectClass=telegramAccount)(objectClass=weeeOpenPerson))"', 'targetattr = "objectClass ||  uid || sn || schacPersonalUniqueCode || degreeCourse || schacDateOfBirth || schacPlaceOfBirth || mobile || mail || safetyTestDate || telegramID || telegramNickname || description"'), {'add', 'write', 'delete'}, f'groupdn = "ldap:///cn=HR,ou=Groups,{suffix}"'))
 	print_aci(make_aci('Allow HR to change users password', ('targetfilter = "(uid=*)"', 'targetattr = "userPassword"'), {'add', 'write'}, f'groupdn = "ldap:///cn=HR,ou=Groups,{suffix}"'))
 
 	print(f"""      -
         dn: "ou=Groups,{suffix}"
         acis:""")
-	print_aci(make_aci('Allow WSO2IS to read groups', ('targetfilter = "(cn=*)"', 'targetattr = "objectClass || cn || ou || description || member || uniqueMember  || createTimestamp || creatorsName || entrydn || entryid || hasSubordinates || modifiersName || modifyTimestamp || nsUniqueId || numSubordinates || parentid || subschemaSubentry"'), {'read', 'search', 'compare'}, f'userdn = "ldap:///cn=WSO2IS,ou=Services,{suffix}"'))
-	print_aci(make_aci('Allow HR to read groups', ('targetfilter = "(cn=*)"', 'targetattr = "objectClass || cn || ou || description || member || uniqueMember"',), {'read', 'search', 'compare'}, f'groupdn = "ldap:///cn=HR,ou=Groups,{suffix}"'))
+	print_aci(make_aci('Allow WSO2IS to read groups', ('targetfilter = "(cn=*)"', 'targetattr = "objectClass || cn || ou || description || member || uniqueMember || nsUniqueId"'), {'read', 'search', 'compare'}, f'userdn = "ldap:///cn=WSO2IS,ou=Services,{suffix}"'))
+	print_aci(make_aci('Allow Nextcloud to read groups', ('targetfilter = "(cn=*)"', 'targetattr = "objectClass || cn || ou || description || member || uniqueMember || nsUniqueId"'), {'read', 'search', 'compare'}, f'userdn = "ldap:///cn=Nextcloud,ou=Services,{suffix}"'))
+	print_aci(make_aci('Allow HR to read groups', ('targetfilter = "(cn=*)"', 'targetattr = "objectClass || cn || ou || description || member || uniqueMember || nsUniqueId"',), {'read', 'search', 'compare'}, f'groupdn = "ldap:///cn=HR,ou=Groups,{suffix}"'))
 	print_aci(make_aci('Allow HR to add and remove people from groups', ('targetfilter = "(cn=*)"', 'targetattr = "member || uniqueMember"',), {'write'}, f'groupdn = "ldap:///cn=HR,ou=Groups,{suffix}"'))
 
 
